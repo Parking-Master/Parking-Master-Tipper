538e20f85fe56c9d4bb65ec345fdd2652b78e1e6
Parking Master <parkingmaster@email.com>
2022-06-27 11:00:02 -0400

    Fix function problem on w.i.n (Root/tipper.js)

--- tipper.js
+++ tipper.js
@@ -6,6 +6,7 @@ if (!w.i) {
   w.i.n = (msg) => new Notification("Parking Master 2.0", { body: msg });
 } else {
   w.i.n = w.i.Notify;
+  w.i.n.init({info:{background:"#fff",textColor:"#ddd",notiflixIconColor:"#ddd"}});
 }
 const tips = [{ type: "tip", text: "Use keyboard shortcuts like the navigation arrows to drive the car" }, { type: "tip", text: "Gamepads like Xbox and PlayStation controllers are supported too" }, { type: "reminder", text: "Signup for a free account and save your points wherever you go" }, { type: "reminder", text: "Use the Autopark (A) Option in all levels for autoparking" }, { type: "reminder", text: "Play world-wide games with the multiplayer option in the main menu." }];
 function tip() {
@@ -14,4 +15,4 @@ function tip() {
   let result = prefix + " " + tipChosen.text;
   return(result);
 }
-w.i.n(tip());
+w.i.n.info(tip());
